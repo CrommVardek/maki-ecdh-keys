ad2f7feeff58242c6f1d840d75adfbbec72334a3
Cyril Carlier <cyrilcarlier@hotmail.com>
2023-07-20 22:02:27 +0200

    Working example of shared key using k256 (elliptic_curves)

--- ecdh-key-gen/src/ecdh_key_gen.rs
+++ ecdh-key-gen/src/ecdh_key_gen.rs
@@ -10,6 +10,9 @@ use ring::agreement::X25519;
 use ring::error::Unspecified;
 use ring::rand::SystemRandom;
 
+use sha2::Sha256;
+
+use hex_literal::hex as hex_macro;
 use rand_core::OsRng;
 
 use hex;
@@ -30,7 +33,7 @@ pub fn generate_ecdh_key_pair() -> Result<(), ring::error::Unspecified> {
     Ok(())
 }
 
-pub fn generate_pub_key_k256() {
+pub fn generate_shared_key_k256() {
     let a = EphemeralSecret::random(&mut OsRng);
     let A = EncodedPoint::from(a.public_key());
 
@@ -46,4 +49,33 @@ pub fn generate_pub_key_k256() {
 
     let Bbytes = B.as_ref();
     println!("\nBob public key {:x?}", hex::encode(Bbytes));
+
+    let Abytes = A.as_ref();
+    println!("\nAlice public key {:x?}", hex::encode(Abytes));
+
+    let Bbytes = B.as_ref();
+    println!("\nBob public key {:x?}", hex::encode(Bbytes));
+    let mut okm = [0u8; 42];
+
+    let alice_shared = a.diffie_hellman(&bob_public);
+    let bob_shared = b.diffie_hellman(&alice_public);
+
+    let salt = hex_macro!("000102030405060708090a0b0c");
+    let info = hex_macro!("f0f1f2f3f4f5f6f7f8f9");
+
+    let mut okm = [0u8; 42];
+    let shared1 = alice_shared
+        .extract::<Sha256>(Some(&salt[..]))
+        .expand(&info, &mut okm);
+    println!("\nAlice shared secret key {:x?}", hex::encode(okm));
+
+    let mut okm2 = [0u8; 42];
+    let shared2 = bob_shared
+        .extract::<Sha256>(Some(&salt[..]))
+        .expand(&info, &mut okm2);
+    println!("\nBob shared secret key {:x?}", hex::encode(okm2));
+
+    if okm == okm2 {
+        println!("\nKeys are the same")
+    }
 }
--- ecdh-key-gen/src/main.rs
+++ ecdh-key-gen/src/main.rs
@@ -2,7 +2,7 @@ mod ecdh_key_gen;
 
 fn main() {
     ecdh_key_gen::generate_ecdh_key_pair();
-    ecdh_key_gen::generate_pub_key_k256();
+    ecdh_key_gen::generate_shared_key_k256();
 }
 
 
